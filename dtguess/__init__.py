4074dcfb81d0352da65d042b227163e871dbf2e0
ownport <ownport@gmail.com>
2016-11-02 07:54:51 +0200

    [+] dtguess

---
+++ dtguess/__init__.py
@@ -0,0 +1,16 @@
+
+__title__ = 'dtguess'
+__version__ = "0.0.1"
+
+import os
+import sys
+
+path = os.path.realpath(os.path.abspath(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
+sys.path.insert(0, os.path.join(os.path.dirname(path), 'vendor/'))
+
+
+from types import StringType, IntegerType, DecimalType, BooleanType, \
+                DateType,  DateUtilType
+
+from utils import type_guess
