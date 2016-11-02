334c018534de7c2447b79d6e6fd8a74ebe322556
ownport <ownport@gmail.com>
2016-11-02 07:55:09 +0200

    [+] setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+from dtguess import __title__
+from dtguess import __version__
+
+setup(
+    name=__title__,
+    version=__version__,
+    py_modules=['dtguess'],
+)
