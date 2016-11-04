64c240c982c47f28b140f9a8a3f0b53ce00248e9
ownport <ownport@gmail.com>
2016-11-05 00:30:01 +0200

    [+] handling empty guesses

--- tests/test_cell_guess.py
+++ tests/test_cell_guess.py
@@ -3,10 +3,12 @@ from dtguess import StringType, IntegerType, DecimalType, BooleanType, \
                 DateType,  DateUtilType
 
 from dtguess import CellGuess
+from dtguess import ColumnGuess
+
 
 def test_cell_guess_basic_types():
 
-    assert CellGuess().guess(None) == [BooleanType()]
+    assert CellGuess().guess(None) == []
 
     assert CellGuess().guess(5) == [IntegerType()]
     assert CellGuess().guess('5') == [IntegerType()]
@@ -18,3 +20,12 @@ def test_cell_guess_basic_types():
     assert CellGuess().guess('2016-01-01') == [StringType()]
     assert CellGuess().guess('2016-01-01 01:02:63') == [StringType()]
     assert CellGuess().guess('2016-01-01 01:02:03') == [DateType('%Y-%m-%d %H:%M:%S')]
+
+
+def test_cell_guess_column_guess():
+
+    column = [1,2,3,4,5]
+    cg = ColumnGuess()
+
+    assert cg.guess(column) == (5, {IntegerType(): 5})
+    assert cg.stats() == (5, {IntegerType(): 5})
