46673889722dd4ee302852f2197d8431dba01403
ownport <ownport@gmail.com>
2016-11-05 00:28:15 +0200

    [+] handling empty guesses

--- dtguess/guesses.py
+++ dtguess/guesses.py
@@ -3,7 +3,7 @@
 from utils import get_default_instances
 
 
-class CellGuess():
+class Guess(object):
 
     def __init__(self, type_instances=get_default_instances()):
 
@@ -11,11 +11,50 @@ class CellGuess():
             raise RuntimeError('Type instances shall be list or tuple of types, %s' % type(type_instances))
         self._type_instances = type_instances
 
+
+class CellGuess(Guess):
+
     def guess(self, value):
 
         guesses = []
         for t in self._type_instances:
             if t.test(value):
                 guesses.append((t.guessing_weight, t))
-        max_weight = max([g[0] for g in guesses])
-        return [g[1] for g in guesses if g[0] == max_weight]
+        if len(guesses) > 0:
+            max_weight = max([g[0] for g in guesses])
+            return [g[1] for g in guesses if g[0] == max_weight]
+        else:
+            return []
+
+
+class ColumnGuess(Guess):
+
+
+    def __init__(self, type_instances=get_default_instances()):
+
+        super(ColumnGuess, self).__init__(type_instances)
+        self._cells = 0
+        self._guesses = dict()
+
+
+    def guess(self, column):
+
+        if not isinstance(column, (list, tuple)):
+            raise RuntimeError('The column shall be a list or tuple of cells, %s' % type(column))
+
+        cell_guess = CellGuess(self._type_instances)
+        for ci, cell in enumerate(column):
+            for _cell_type in cell_guess.guess(cell):
+                if _cell_type not in self._guesses:
+                    self._guesses[_cell_type] = 1
+                else:
+                    self._guesses[_cell_type] += 1
+
+        self._cells = ci + 1 if ci else 0
+        return self._cells, self._guesses
+
+
+    def stats(self):
+        ''' return guessing stats
+        '''
+        return self._cells, self._guesses
