c1fde266fd06c189ccb9522d22e82f34e87b80b0
ownport <ownport@gmail.com>
2016-11-06 23:56:19 +0200

    [+] package metadata

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup
+
+try:
+    from setuptools import setup, find_packages
+except ImportError, err:
+    raise ImportError('Cannot import setup() and find_packages(), %s' % err)
 
 from dtguess import __title__
 from dtguess import __version__
@@ -6,8 +10,14 @@ from dtguess import __version__
 setup(
     name=__title__,
     version=__version__,
+    description='Python library for data type guessing',
+    author="Andrey Usov",
+    author_email="ownport@gmail.com",
+    url='https://github.com/ownport/dtguess',
     py_modules=['dtguess'],
+    packages=find_packages(),
     install_requires=[
-        'python-dateutil', 'six'
+        'six',
+        'python-dateutil',
     ]
 )
